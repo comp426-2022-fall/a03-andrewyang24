c570014efd137bd4a0239290865d96417bc2d0dc
Andrew Yang <ayang24@email.unc.edu>
2022-11-23 18:02:21 -0500

    modified rolls

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { roll } from "/lib/roll.js";
+import { roll } from "../lib/roll.js";
 import minimist from 'minimist';
 
 const args = minimist(process.argv.slice(2))
 
-console.log(JSON.stringify(roll(argv.sides, argv.dice, argv.rolls)))
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)))
